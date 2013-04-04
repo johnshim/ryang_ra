874185c82d2e5b966935b04427db03c1bf522f78
R Yang <yang.runnan@gmail.com>
2013-04-03 22:05:09 -0500

    changed to use WHERE for speed

--- scripts/filter.py
+++ scripts/filter.py
@@ -5,16 +5,20 @@ from front_month import *
 import datetime as dt
 import tables
 import numpy
+import time
+import sys
 
 from tables import *
 
 # VARS
 DATA_FOLDER = "/media/sf_Dropbox/Work/budish_ra/data/"
-DATE = dt.date(2011,10,16) # is the day before - weird
+DATE = dt.date(2011,10,17) 
 
 START_TIME = dt.time(13, 59, 59, 999999) # 9am = 1400 GMT
-#END_TIME = dt.time(20, 00, 00, 1) # 3pm = 2000 GM
-END_TIME = dt.time(14, 00, 05, 1) # 3pm = 2000 GMT
+#END_TIME = dt.time(17, 00, 00, 1) # 3pm = 2000 GM
+END_TIME = dt.time(15, 00, 05, 1) # 3pm = 2000 GMT
+
+NAME = "1"
 
 ############################
 # Dan's definitions
@@ -39,7 +43,7 @@ class ImpliedBookTable(tables.IsDescription):
     ask         = Int64Col(shape=(10,2))
     bid         = Int64Col(shape=(10,2))
     implied     = Int32Col()
-
+    
 class TradeTable(tables.IsDescription):
     timestamp   = Int64Col()
     timestamp_s = StringCol(16)
@@ -50,16 +54,13 @@ class TradeTable(tables.IsDescription):
 
 ############################
 ############################
-
+"""
 def tests():
     fail = 0
 
     if not fail:
         print "ALL TESTS PASSED"
-
-
-
-
+"""
 def to_ts(ts):
     return dt.datetime.fromtimestamp(float(ts)/1000000)
 
@@ -75,29 +76,37 @@ def filter():
     contract_name = get_front_month(DATE)
     front_contract = getattr(f.root, contract_name)
 
-    print contract_name
+    print "Filtering for front month contract: ", contract_name
+    print "Filtering for time: ", START_TIME, END_TIME
+
+    #print contract_name
     #print front_contract
 
-    # TODO: REWRITE USING WHERE STATEMENTS FOR EFFICIENCY
-
     # filter by time
-    selected_books = [x[:] for x in front_contract.books if (to_ts(x['timestamp']).time() > START_TIME and to_ts(x['timestamp']).time() < END_TIME)]
 
-    selected_trades = [x[:] for x in front_contract.trades if (to_ts(x['timestamp']).time() > START_TIME and to_ts(x['timestamp']).time() < END_TIME)]
+    init = time.mktime(dt.datetime.combine(DATE, START_TIME).timetuple()) * 1000000
+    end = time.mktime(dt.datetime.combine(DATE, END_TIME).timetuple()) * 1000000
+
+    print init, end, end - init
+
+    selected_books = front_contract.books.readWhere('(timestamp >= init) & (timestamp <= end)')
+    selected_trades = front_contract.trades.readWhere('(timestamp >= init) & (timestamp <= end)')
 
     # setup file for export
+    filter_settings = Filters(complevel = 1)
+    print "Writing to: ", DATA_FOLDER + date + "f"
     f2 = tables.openFile(DATA_FOLDER + date + "f", mode = "w", title = "outfile")
 
     # Changed to have same group across diff front mth contracts
     group = f2.createGroup("/", "ES", "ES") 
-    #group = f2.createGroup("/", contract_name, contract_name)
     
-    table = f2.createTable(group, "books", BookTable, "books books")
+    table = f2.createTable(group, "books", BookTable, "books books", filters = filter_settings)
     table.append(selected_books)
 
-    table2 = f2.createTable(group, "trades", TradeTable, "traes trdes")
+    table2 = f2.createTable(group, "trades", TradeTable, "trades trades", filters = filter_settings)
     table2.append(selected_trades)
 
+    f.close()
     f2.close()    
 
 if __name__ == "__main__":
