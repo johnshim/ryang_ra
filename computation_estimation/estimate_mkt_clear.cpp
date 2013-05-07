0208733e32c27eb2aafd1036172cf46f2af469e1
R Yang <yang.runnan@gmail.com>
2013-05-07 00:03:10 -0500

    switched to vector, large speed improvements

--- computation_estimation/estimate_mkt_clear.cpp
+++ computation_estimation/estimate_mkt_clear.cpp
@@ -2,21 +2,19 @@
 #include<random>
 #include<vector>
 #include<map>
+#include<algorithm>
 
 #include <time.h>
 
 using namespace std;
 
-int main(){
-
-  // Parameters
-  const int n = 100000;
-
+int test(int n){
 
   // generate random numbers
-  default_random_engine gen;
-  normal_distribution<double> distbid(1000, 5);
-  normal_distribution<double> distask(1010, 5);
+  random_device rd;
+  default_random_engine gen(rd());
+  normal_distribution<double> distbid(78600, 10000);
+  normal_distribution<double> distask(78505, 10000);
 
   vector<int> bids;
   vector<int> asks;
@@ -26,57 +24,78 @@ int main(){
     asks.push_back(floor(distask(gen)));
   }
 
+  // Aggregate S/D into lists [price:qty]
+  timespec t0;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t0);
+  
+  int foomaxbid = *max_element(bids.begin(), bids.end());
+  //int foominbid = *min_element(bids.begin(), bids.end());
+  int foomaxask = *max_element(asks.begin(), asks.end());
+  int foominask = *min_element(asks.begin(), asks.end());
+  
+  vector<unsigned int> foob(foomaxbid+1);
+  vector<unsigned int> fooa(foomaxask+1);
+
+  for (unsigned int i = 0; i < bids.size(); i++){
+    foob[bids[i]]++;
+  }
+  
+  for (unsigned int i = 0; i < bids.size(); i++){
+    fooa[asks[i]]++;
+  }
+  
   timespec t1, t2;
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t1);
 
-  // Aggregate S/D into Maps
-  map<int, int> bidMap;
-  map<int, int> askMap;
-
-  for (int i = 0; i < bids.size(); i++){
-    bidMap[bids[i]]++;
-  }
-
-  for (int i = 0; i < asks.size(); i++){
-    askMap[asks[i]]++;
-  }
-
   // Find Market Clearing Prices
 
-  cout << bidMap.begin()->first << " " << bidMap.rbegin()->first << endl;
-  cout << askMap.begin()->first << " " << askMap.rbegin()->first << endl;
+  int maxbid = foomaxbid;
+  int minask = foominask;
 
-  int maxbid = bidMap.rbegin()->first;
-  int minask = askMap.begin()->first;
-
-  int demand = 0;
-  int supply = 0;
+  int demand = -1;
+  int supply = -1;
   int prevd = 0;
   int prevs = 0;
 
   int guess = 0;
-
+  int prevg = 0;
+  
   if (maxbid > minask){
     guess = int(floor((maxbid + minask) / 2.0));
+    cout << "GS: \t" << guess << endl;
+    //guess = minask;
 
     while (1 == 1){
+
       prevd = demand;
       prevs = supply;
 
-      demand = 0;
-      supply = 0;
+      if (demand == -1 || supply == -1){
+	demand = 0;
+	supply = 0;
 
-      for (int i = guess; i <= maxbid; i++){
-	map<int, int>::iterator it = bidMap.find(i);
-	if (it != bidMap.end())
-	  demand += bidMap[i];
+	for (int i = guess; i <= maxbid; i++){
+	  demand += foob[i];
+	}
+
+	for (int i = minask; i <= guess; i++){
+	  supply += fooa[i];
+	}
+      }
+      else{
+	// if price goes up
+	if (prevg < guess){
+	  supply += fooa[guess];
+	  demand -= foob[prevg];
+	}
+	// if price goes down
+	else if (prevg > guess){
+	  supply -= fooa[prevg];
+	  demand += foob[guess];
+	}
       }
 
-      for (int i = minask; i <= guess; i++){
-	map<int, int>::iterator it = askMap.find(i);
-	if (it != askMap.end())
-	  supply += askMap[i];
-      }
+      // If the Market crosses, exit
 
       if (demand == supply)
 	break;
@@ -97,24 +116,70 @@ int main(){
 	break;
       }
 
+      prevg = guess;
+
       if (demand > supply)
 	guess += 1;
-
-      if (demand < supply)
+      else if (demand < supply)
 	guess -= 1;
-
-
     }
   }
-
+  
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t2);
 
-  timespec temp;
-  temp.tv_sec = t2.tv_sec-t1.tv_sec;
-  temp.tv_nsec = t2.tv_nsec-t1.tv_nsec;
+  // Calculate the times for each section of the computation
+  
+  timespec temp, tempCLEAR, tempAGG2;
 
-  cout<<temp.tv_sec<<":"<<temp.tv_nsec / 1000000<<endl;
+  // Total
+  temp.tv_nsec = t2.tv_nsec-t0.tv_nsec;
+  if (temp.tv_nsec < 0){
+    temp.tv_nsec = 1000000000 + temp.tv_nsec;
+    temp.tv_sec = t2.tv_sec-t0.tv_sec - 1;
+  }
+  else{
+    temp.tv_sec = t2.tv_sec-t0.tv_sec;    
+  }
 
-  cout << guess << endl;
+  // Aggregation
+  tempAGG2.tv_nsec = t1.tv_nsec-t0.tv_nsec;
+  if (tempAGG2.tv_nsec < 0){
+    tempAGG2.tv_nsec = 1000000000 + tempAGG2.tv_nsec;
+    tempAGG2.tv_sec = t1.tv_sec-t0.tv_sec - 1;
+  }
+  else{
+    tempAGG2.tv_sec = t1.tv_sec-t0.tv_sec;    
+  }
 
+
+  // Clearing
+  tempCLEAR.tv_nsec = t2.tv_nsec-t1.tv_nsec;
+  if (tempCLEAR.tv_nsec < 0){
+    tempCLEAR.tv_nsec = 1000000000 + tempCLEAR.tv_nsec;
+    tempCLEAR.tv_sec = t2.tv_sec-t1.tv_sec - 1;
+  }
+  else{
+    tempCLEAR.tv_sec = t2.tv_sec-t1.tv_sec;    
+  }
+
+  cout<<"Computation Time: "<<temp.tv_sec<<"s \t : \t "<<temp.tv_nsec / 1000000.0<<"ms"<<endl;
+  cout<<"Aggregation Time: "<<tempAGG2.tv_sec<<"s \t : \t "<<tempAGG2.tv_nsec / 1000000.0<<"ms"<<endl;
+  cout<<"Clearing Time: "<<tempCLEAR.tv_sec<<"s \t : \t "<<tempCLEAR.tv_nsec / 1000000.0<<"ms"<<endl;
+  cout << "maxbid:\t"<<maxbid<<"\t minask:\t"<<minask<<endl;
+  cout <<"Market Clearing Price: "<< guess << endl;
+  cout << "Demand: " << demand << "\t Supply: "<<supply << endl;
+
+  return tempCLEAR.tv_nsec / 1000000.0;
+}
+
+int main(){
+  //double sum = 0;
+  
+  for (int i = 1; i < 7; i++){
+    cout << "M = " << pow(10,i) << endl;
+    test(pow(10,i));
+  }
+  
+
+  //test(pow(10,5));
 }
