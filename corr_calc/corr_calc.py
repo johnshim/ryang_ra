25d513cb259f38b929795c09b3982ce890691f55
R Yang <yang.runnan@gmail.com>
2013-05-28 01:27:09 -0500

    add getting futures data from quandl

--- corr_calc/corr_calc.py
+++ corr_calc/corr_calc.py
@@ -32,11 +32,13 @@ def getETFTicks():
     return tickers
 
 #
+# Begin External Code
 # Source: http://jiripik.me/2012/06/06/python-code-for-getting-market-data-from-finance-yahoo-com/
 #
 
 # prices = data[:,6] or prices = data[:, title.index("Adj Close")], pl.num2date(data[:,1]) back dates
 # syntax http://ichart.yahoo.com/table.csv?s={Yahoo.Symbol.[isin]}&a={Von.M-1}&b={Von.T}&c={Von.J}&d={Bis.M}&e={Bis.T}&f={Bis. J}&g=d&y=0&z=jdsu&ignore=.csv
+
 def getNumpyHistoricalTimeseries(symbol,fromDate, toDate):
     f = urllib2.urlopen('http://ichart.yahoo.com/table.csv?a='+ str(fromDate.month -1) +'&c='+ str(fromDate.year) +'&b=' + str(fromDate.day) + '&e='+  str(toDate.day) + '&d='+ str(toDate.month-1) +'&g=d&f=' + str(toDate.year) + '&s=' + symbol + '&ignore=.csv')
     header = f.readline().strip().split(",")
@@ -45,6 +47,10 @@ def getNumpyHistoricalTimeseries(symbol,fromDate, toDate):
 #
 # End External Code
 #
+
+#####
+# Get Stocks/ETFs from Yahoo Finance
+#####
 
 def getStockData(symbol, fromDate, toDate):
     n = getNumpyHistoricalTimeseries(symbol, d1, d2)
@@ -66,21 +72,80 @@ def getStockData(symbol, fromDate, toDate):
 
     return (pd, volume, volatility)
 
+#####
+# Get Futures from Quandl
+#####
 
 
+def getQuandlFuturesData(symbol, d1, d2):
+
+    year1 = str(d1.year)
+    if d1.month > 9:
+        month1 = str(d1.month)
+    else:
+        month1 = "0" + str(d1.month)
+    if d1.day > 9:
+        day1 = str(d1.day)
+    else:
+        day1 = "0" + str(d1.day)
+
+    year2 = str(d2.year)
+    if d2.month > 9:
+        month2 = str(d2.month)
+    else:
+        month2 = "0" + str(d2.month)
+    if d2.day > 9:
+        day2 = str(d2.day)
+    else:
+        day2 = "0" + str(d2.day)
+
+    url = "http://www.quandl.com/api/v1/datasets/OFDP/FUTURE_" + symbol + ".csv?&trim_start=" + year1 + "-" + month1 + "-" + day1 + "&trim_end=" + year2 + "-" + month2 + "-" + day2 + "&sort_order=desc"
+
+    f = urllib2.urlopen(url)
+    head = f.readline().strip().split(",")
+    data = np.loadtxt(f, dtype=np.float, delimiter=",", converters={0: pl.datestr2num})
+    
+    return data
+
+def getFuturesData(symbol, d1, d2):
+
+    data = getQuandlFuturesData(symbol, d1, d2)
+    
+    volume = np.mean(data[:,5])
+    settle = data[:,4]
+    
+    diff = np.diff(settle)
+    pd = diff / settle[:-1]
+    
+    volatility = np.std(pd)
+
+    return (pd, volume, volatility)
+
+def getCorrMatrix(returns, write=False):
+
+    # Filter out all tickers with errors
+    returns = returns[~np.all(returns == 0, axis = 1)]
+
+    if (write == True):
+        np.savetxt("corr_calc.out", returns, delimiter=',')
+
+    return np.corrcoef(returns)
 
 if __name__ == "__main__":
 
-    #stocks = ["GOOG", "AAPL", "YHOO", "MSFT", "NFLX", "DIS", "GS", "JPM"]
+    # Parameters
+    minVolume = 10000000
+    minCorr = 0.7
+
     stocks = getSPTicks()
     ticks = []
 
     etfs = getETFTicks()
 
-    stocks = etfs
+    stocks = stocks + etfs
 
-    d1 = datetime.date(2012,1,1)
-    d2 = datetime.date(2013,1,1)
+    d1 = datetime.date(2011,1,1)
+    d2 = datetime.date(2012,1,1)
 
     # Get the # of entries
     try:
@@ -88,8 +153,11 @@ if __name__ == "__main__":
     except:
         print stocks[0]
     
-    #print "Stock \tVolume \tVolatility"
+    print "Ticker \tVolume \tVolatility"
     returns = np.zeros([len(stocks),obs])
+    
+    ticks = {}
+    ticklist = []
 
     for s in xrange(len(stocks)):
         try:
@@ -98,6 +166,14 @@ if __name__ == "__main__":
             # Usual problem: ticker was not live for full year
             print stocks[s], " ERROR"
             continue
+
+
+        #
+        # Filter for volume threshold
+        #
+        if x[1] < minVolume:
+            continue
+
         print stocks[s], '\t', x[1], '\t', x[2]
 
         try:
@@ -108,13 +184,27 @@ if __name__ == "__main__":
             continue
 
         # If successful, add to list of ticks that went through
-        ticks.append(stocks[s])
+        ticks[stocks[s]] = {'volume':x[1], 'volatility':x[2]}
+        ticklist.append(stocks[s])
 
-    out = open('corr_calc.out', 'w')
+    corr = getCorrMatrix(returns)
+    
+    entries = []
 
-    # Filter out all tickers with errors
-    returns = returns[~np.all(returns == 0, axis = 1)]
+    # row i, col j (upper triangle only)
+    for i in xrange(corr.shape[0]):
+        for j in xrange(i+1, corr.shape[1]):
+            if corr[i,j] > minCorr:
+                t1 = ticklist[i]
+                t2 = ticklist[j]
+                entries.append([t1, t2, corr[i,j], ticks[t1]['volume'], ticks[t2]['volume'], ticks[t1]['volatility'], ticks[t2]['volatility']])
 
-    np.savetxt("corr_calc.out", returns, delimiter=",")
+    for i in entries:
+        print i
+            
+    csvout = open('corr_calc.out.csv', 'w')
+    writer = csv.writer(csvout)
+    writer.writerow(['Ticker1', 'Ticker2', 'Corr', 'Vlm1', 'Vlm2', 'Vol1', 'Vol2'])
+    writer.writerows(entries)
 
-    print np.corrcoef(returns)
+    csvout.close()
