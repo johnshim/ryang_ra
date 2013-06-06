cbc1478bb17cbfcd15ec213a567707b3925a7f79
R Yang <yang.runnan@gmail.com>
2013-06-06 12:00:51 -0500

    Update getFutures

--- corr_calc/getFutures.m
+++ corr_calc/getFutures.m
@@ -10,16 +10,22 @@ function [] = getFutures(ticker, startdate, enddate)
     c = blp;
 
     % Download the requisite data for the ticker and date range
-    [d] = history(c, ticker, 'LAST_PRICE', startdate, enddate)
+    [d] = history(c, ticker, 'LAST_PRICE', startdate, enddate);
     settle = d(:,2);
     
-    [d] = history(c, ticker, 'VOLUME', startdate, enddate)
+    [d] = history(c, ticker, 'VOLUME', startdate, enddate);
     vol = d(:,2);
-
+    
+    size = getdata(c, ticker, 'FUT_CONT_SIZE');
+    size = size(1).FUT_CONT_SIZE;
+    
     date = d(:,1);
     
+    % Multiply by nominal
+    vol = vol .* size;
+    
     % Save to csv to be read by python
-    dlmwrite(['blp_data_', ticker, 'temp.csv'], flipud([date,settle,vol]), 'delimiter', ',', 'precision', 9);
+    dlmwrite(['blp_data_', ticker, '.csv'], flipud([date,settle,vol]), 'delimiter', ',', 'precision', 9);
     
     % Close connection
     close(c);
